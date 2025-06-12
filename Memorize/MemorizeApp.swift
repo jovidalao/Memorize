4033de09d22545e13e386cc604ae46adb38f280f
jovidalao <jovidalao@gmail.com>
2025-06-12 13:22:40 +1000

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by jovi on 12/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemorizeTests/MemorizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeTests.swift
+//  MemorizeTests
+//
+//  Created by jovi on 12/6/2025.
+//
+
+import Testing
+@testable import Memorize
+
+struct MemorizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
